f2437d156685454dfc2ff31a317d7bb45762a242
cheryl-azyra <cheryl.dunn@azyra.com>
2025-11-14 08:24:50 -0500

    Add GUI to project

---
+++ HashFilesApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HashFilesLib;
+
+namespace HashFilesApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Triggered when the user selects Hash button
+        // Calls class library to hash all files from sourcePath and output them to the destinationPath
+        public void GenerateHashedFile(object sender, RoutedEventArgs e)
+        {
+
+            ResultText.Content = HashFilesLib.HashFiles.GetHashedFiles(sourcePathTB.Text, destinationPathTB.Text);
+            ResultText.Visibility = Visibility.Visible;
+
+        }
+    }
+}
